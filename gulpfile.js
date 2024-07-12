23e86fac11cb949dad11e53e98fee3825beb4c8c
SkalZ <dima.tysyahnyy@mail.ru>
2024-07-12 18:22:39 +0500

    start

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,10 @@
 import gulp from 'gulp';
-import * as dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-const sass = gulpSass(dartSass)
-import sourcemaps from 'gulp-sourcemaps';
-import watch from 'gulp-watch';
-import browserSync from 'browser-sync';
+import server from './gulp/tasks/server.js';
+import { pugWatch } from './gulp/tasks/pug.js';
+import { sassWatch } from './gulp/tasks/sass.js';
 
-
-// Компиляция SASS
-gulp.task('sass-compile', function(){
-    return gulp.src('site/scss/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError)) 
-    .pipe(sourcemaps.write('site/'))
-    .pipe(gulp.dest('site/css/'))
-    .pipe(browserSync.stream())
-})
-
-// Auto-reload
-gulp.task('default', function(){
-    browserSync.init({
-        server: {
-            baseDir: 'site/'
-        },
-    notify: false
-    });
-  watch('site/scss/**/*.scss', gulp.series('sass-compile'));
-  watch('site/*.html').on('change', browserSync.reload);
-})
+gulp.task('default', gulp.parallel(
+    server,
+    sassWatch,
+    pugWatch
+))
