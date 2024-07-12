5ac4cbaf3c025645a487b93af7fabae09541887b
SkalZ <dima.tysyahnyy@mail.ru>
2024-07-12 22:28:53 +0500

    header ready

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,10 @@
 export default {
-    extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
-    rules: {
-      "at-rule-no-unknown": [true, {
-        "ignoreAtRules": ["mixin", "include"]
-      }]
-    }
+  extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
+  rules: {
+    "at-rule-no-unknown": [true, {
+      "ignoreAtRules": ["mixin", "include"]
+    }],
+    "import-notation": null,
+    "no-descending-specificity": null
   }
+}
